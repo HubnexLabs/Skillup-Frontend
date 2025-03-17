e7bf0d9c29223579d59a8d8d82cebc0e88fef5f9
Shubham Liman <shubhamlstudymail@gmail.com>
2025-03-17 22:06:12 +0530

    update skillset carsoul

--- src/components/custom/Landing/Hero.tsx
+++ src/components/custom/Landing/Hero.tsx
@@ -5,7 +5,7 @@ import Learner from "@/components/shared/Learner";
 const Hero = () => {
   return (
     <>
-      <div className="w-full h-auto relative overflow-hidden p-5 ">
+      <div className="flex justify-center w-full h-auto relative overflow-hidden p-5 ">
         <img
           src="/icons/rounds.svg"
           className="absolute left-0 top-0"
@@ -20,15 +20,15 @@ const Hero = () => {
         <span className="absolute bottom-0 right-[20%] -translate-x-[20%] w-[15px] h-[15px] bg-green-one rounded-full"></span>
         <span className="absolute left-20 top-[20%] -translate-x-[20%] w-[15px] h-[15px] bg-green-one rounded-full"></span>
         <span className="absolute left-0 bottom-0 w-[160px] h-[160px] bg-green-radial blur-[100px] rounded-full"></span>
-        <div className="flex flex-col lg:flex-row justify-between w-full lg:w-[90%] mx-auto p-5 gap-5">
-          <div className="w-full text-left flex flex-col items-center lg:w-[50%] ">
-            <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold text-nowrap">
+        <div className="flex flex-col lg:flex-row justify-between w-full lg:w-[90%]  p-5 gap-10">
+          <div className="w-full flex justify-center lg:w-[50%] ">
+            {/* <h1 className="text-4xl text-left md:text-5xl lg:text-7xl font-bold text-nowrap">
               The <span className="text-green-tertiary">Ultimate</span> <br />
               <span className="text-green-tertiary">Launchpad</span> For <br />
               Your <span className="text-green-tertiary">Career!</span>
             </h1>
 
-            <p className="text-lg my-4 font-light text-black-100">
+            <p className="text-lg my-4 text-left font-light text-black-100">
               Where Your Future Begins: Unlock Guaranteed Career <br /> Success
               With The Best Guidance
             </p>
@@ -47,6 +47,40 @@ const Hero = () => {
                 100% Money Back Guarantee if you're not satisfied with the
                 Evaluation
               </p>
+            </div> */}
+            <div className="flex flex-col">
+              <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold text-nowrap">
+                The <span className="text-green-tertiary">Ultimate</span> <br />
+                <span className="text-green-tertiary">Launchpad</span> For{" "}
+                <br />
+                Your <span className="text-green-tertiary">Career!</span>
+              </h1>
+
+              <p className="text-lg my-4 text-left font-light text-black-100">
+                Where Your Future Begins: Unlock Guaranteed Career <br />{" "}
+                Success With The Best Guidance
+              </p>
+
+              <Learner />
+
+              <div className="flex gap-2 my-4 ">
+                <Button
+                  text="Enroll Now"
+                  classname="py-3 px-4 lg:px-8 lg:py-3"
+                />
+                <ShinyButton className="border rounded-md py-3">
+                  <span className="text-xs md:text-base text-nowrap text-black font-semibold py-1">
+                    Get Free Career Evaluation
+                  </span>
+                </ShinyButton>
+              </div>
+              <div className="flex my-6 text-center">
+                <img src="/icons/success.svg" alt="success" />
+                <p className=" text-sm lg:font-medium">
+                  100% Money Back Guarantee if you're not satisfied with the
+                  Evaluation
+                </p>
+              </div>
             </div>
           </div>
           <div className="lg:w-[50%] w-full">
